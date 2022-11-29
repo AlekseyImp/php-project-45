d6804d6b83e732479bba4c8423093952e0a28996
Aleksey Petras <leha.petras1998@gmail.com>
2022-11-29 16:53:04 +0300

    minnor fixes

--- src/Cli.php
+++ src/Cli.php
@@ -2,13 +2,21 @@
 
 namespace BrainGames\Cli;
 
-function line($string, $userName = '')
+function line($string)
 {
-    echo "{$string}{$userName}\n";
+    echo "{$string}\n";
 }
 function prompt($string)
 {
     echo "$string ";
     $userName = trim(fgets(STDIN));
+    return $userName;
+}
+function greetings()
+{
+    line("Welcome to the Brain Game!");
+    line("May I have your name? ");
+    $userName = trim(fgets(STDIN));
+    line("Hello, {$userName}");
     return $userName;
 }
