892d0a37d975bf085a1f56d84ab88ed3d90e1a97
Aleksey Petras <leha.petras1998@gmail.com>
2022-12-10 19:36:28 +0300

    Shorten code as much i invented

--- src/Brain-prime.php
+++ src/Brain-prime.php
@@ -2,50 +2,24 @@
 
 namespace BrainGames\Brain\prime;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use function BrainGames\Cli\greetings;
 use function BrainGames\Cli\line;
 use function BrainGames\Cli\isPrime;
+use function BrainGames\Cli\isCorrectAnswer;
 
 function brainPrime()
 {
     $userName = greetings();
     $count = 0;
+    line("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
     for ($i = 0; $i < 3; $i++) {
         $primeNum = rand(2, 103);
-        $answer = '';
-        line("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
         line("Question: {$primeNum}");
         line("Your answer: ", false);
+        $correctAnswer = isPrime($primeNum);
         $userAnswer = trim(fgets(STDIN));
-        if (isPrime($primeNum)) {
-            $answer = 'yes';
-            if ($answer == $userAnswer) {
-                line("Correct!");
-                $count++;
-            } else {
-                line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$answer}'");
-                line("Let`s try again, {$userName}!");
-                break;
-            }
-        } else {
-            $answer = 'no';
-            if ($answer == $userAnswer) {
-                line("Correct!");
-                $count++;
-            } else {
-                line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$answer}'");
-                line("Let`s try again, {$userName}!");
-                break;
-            }
-        }
+        isCorrectAnswer($userAnswer, $correctAnswer, $userName);
+        $count++;
     }
     if ($count === 3) {
         line("Congratulations, {$userName}");
