09b94e04be119457b0a6c1c0d1a38e7cbacc18dd
Aleksey Petras <leha.petras1998@gmail.com>
2022-12-10 19:36:47 +0300

    Shorten code as much i invented

--- src/Brain-even.php
+++ src/Brain-even.php
@@ -2,46 +2,33 @@
 
 namespace BrainGames\Brain\even;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use function BrainGames\Cli\greetings;
 use function BrainGames\Cli\line;
+use function BrainGames\Cli\isCorrectAnswer;
+
 function brainEven()
 {
     $userName = greetings();
     $count = 0;
+    line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
     for ($i = 0; $i < 3; $i++) {
         $num = rand(1, 50);
         line("Question: {$num}");
         line("Your answer: ", false);
-        $answer = trim(fgets(STDIN));
-        if ($num % 2 === 0) {
-            if ($answer === 'yes') {
-                line("Correct!");
-                $count++;
-            } else {
-                line("{$answer} is wrong answer ;(. Correct answer was 'yes'");
-                line("Let's try again, {$userName}!");
-                break;
-            }
-        } else {
-            if ($answer === 'no') {
-                line("Correct!");
-                $count++;
-            } else {
-                line("{$answer} is wrong answer ;(. Correct answer was 'no'");
-                line("Let's try again, {$userName}!");
-                break;
-            }
-        }
+        $correctAnswer = getCorrectAnswer($num);
+        $userAnswer = trim(fgets(STDIN));
+        isCorrectAnswer($userAnswer, $correctAnswer, $userName);
+        $count++;
     }
     if ($count === 3) {
         line("Congratulations, {$userName}!");
+    }
+}
+function getCorrectAnswer($num)
+{
+    if ($num % 2 === 0) {
+        return 'yes';
+    } else {
+        return 'no';
     }
 }
