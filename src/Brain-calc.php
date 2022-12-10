c5b16e7eb33828db67590f756d76712d640ff723
Aleksey Petras <leha.petras1998@gmail.com>
2022-12-10 19:35:51 +0300

    Shorten code as much i invented

--- src/Brain-calc.php
+++ src/Brain-calc.php
@@ -2,71 +2,50 @@
 
 namespace BrainGames\Brain\calc;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use function BrainGames\Cli\greetings;
 use function BrainGames\Cli\line;
+use function BrainGames\Cli\isCorrectAnswer;
 
 function brainCalc()
 {
     $userName = greetings();
     $count = 0;
+    line("What is the result of the expression?");
     for ($i = 0; $i < 3; $i++) {
         $a = rand(1, 20);
         $b = rand(1, 20);
         $operand = rand(1, 3);
-        $userAnswer = 0;
-        if ($operand === 1) {
-            $corrAnswer = $a + $b;
-            line("What is the result of the expression?");
-            line("Question: {$a} + {$b}");
-            line("Your answer: ", false);
-            $userAnswer = trim(fgets(STDIN));
-            if ($corrAnswer == $userAnswer) {
-                line("Correct!");
-                $count++;
-            } else {
-                line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$corrAnswer}'");
-                line("Let`s try again, {$userName}");
-                break;
-            }
-        } elseif ($operand === 2) {
-            $corrAnswer = $a - $b;
-            line("What is the result of the expression?");
-            line("Question: {$a} - {$b}");
-            line("Your answer: ", false);
-            $userAnswer = trim(fgets(STDIN));
-            if ($corrAnswer == $userAnswer) {
-                line("Correct!");
-                $count++;
-            } else {
-                line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$corrAnswer}'");
-                line("Let`s try again, {$userName}");
-                break;
-            }
-        } elseif ($operand === 3) {
-            $corrAnswer = $a * $b;
-            line("What is the result of the expression?");
-            line("Question: {$a} * {$b}");
-            line("Your answer: ", false);
-            $userAnswer = trim(fgets(STDIN));
-            if ($corrAnswer == $userAnswer) {
-                line("Correct!");
-                $count++;
-            } else {
-                line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$corrAnswer}'");
-                line("Let`s try again, {$userName}");
-                break;
-            }
-        }
+        makeQuestion($operand, $a, $b, $userName);
+        $count++;
     }
     if ($count === 3) {
         line("Congratulations, {$userName}!");
+    }
+}
+
+function makeQuestion($operand, $a, $b, $userName)
+{
+    switch ($operand) {
+        case 1:
+            $correctAnswer = $a + $b;
+            line("Question: {$a} + {$b}");
+            line("Your answer: ", false);
+            $userAnswer = trim(fgets(STDIN));
+            isCorrectAnswer($userAnswer, $correctAnswer, $userName);
+            break;
+        case 2:
+            $correctAnswer = $a - $b;
+            line("Question: {$a} - {$b}");
+            line("Your answer: ", false);
+            $userAnswer = trim(fgets(STDIN));
+            isCorrectAnswer($userAnswer, $correctAnswer, $userName);
+            break;
+        case 3:
+            $correctAnswer = $a * $b;
+            line("Question: {$a} * {$b}");
+            line("Your answer: ", false);
+            $userAnswer = trim(fgets(STDIN));
+            isCorrectAnswer($userAnswer, $correctAnswer, $userName);
+            break;
     }
 }
