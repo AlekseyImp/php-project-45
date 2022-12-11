f908281954f659e2203b8d4b9b05445cd66a1a31
Aleksey Petras <leha.petras1998@gmail.com>
2022-12-11 18:58:48 +0300

    add new function showTask

--- src/Cli.php
+++ src/Cli.php
@@ -57,3 +57,23 @@ function getSTDIN(): string
     $string = strval($string);
     return trim($string);
 }
+function showTask(string $string)
+{
+    switch ($string) {
+        case 'even':
+            line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+            break;
+        case 'calc':
+            line("What is the result of the expression?");
+            break;
+        case 'gcd':
+            line("Find the greatest common divisor of given numbers.");
+            break;
+        case 'prime':
+            line("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
+            break;
+        case 'progression':
+            line("What number is missing in the progression?");
+            break;
+    }
+}
