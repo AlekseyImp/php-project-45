39d7a72a02f9a6f5f3d2ad2cd6ce9ed89b120646
Aleksey Petras <leha.petras1998@gmail.com>
2022-12-24 17:36:28 +0300

    add returning type, remove hungarian notation, mod array of correct answers

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,23 +4,25 @@ namespace BrainGames\Brain\calc;
 
 use function BrainGames\Engine\startGame;
 
-function brainCalc()
+function brainCalc(): void
 {
-    $arrayOfCorrectAnswers = [];
+    $questionAndCorrectAnswer = [];
     $operands = ['+', '-', '*'];
     for ($i = 0; $i < 3; $i++) {
         $a = rand(1, 20);
         $b = rand(1, 20);
         $operand = rand(0, 2);
         $correctAnswer = getCorrectAnswer($operand, $a, $b);
-        $arrayOfCorrectAnswers[] = ["{$a} {$operands[$operand]} {$b}" => $correctAnswer];
+        $expression = "{$a} {$operands[$operand]} {$b}";
+        $questionAndCorrectAnswer[] = ['question' => $expression, 'correctAnswer' => $correctAnswer];
     }
-    startGame('calc', $arrayOfCorrectAnswers);
+    $question = "What is the result of the expression?";
+    startGame($questionAndCorrectAnswer, $question);
 }
 
-function getCorrectAnswer(int $operand, int $a, int $b)
+function getCorrectAnswer(int $operands, int $a, int $b): int
 {
-    switch ($operand) {
+    switch ($operands) {
         case 0:
             return $a + $b;
         case 1:
