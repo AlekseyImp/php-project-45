a89ca501bd3fadfdda7bfb295858d0a7657d52db
Aleksey Petras <leha.petras1998@gmail.com>
2023-02-06 15:14:36 +0300

    add const number_of_rounds

--- src/Engine.php
+++ src/Engine.php
@@ -5,6 +5,7 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
+const NUMBER_OF_ROUNDS = 3;
 function startGame(array $round, string $question): void
 {
     line("Welcome to the Brain Games!");
