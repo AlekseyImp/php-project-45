3a59953401db7dad2ac2f7e6ef6710a37312654a
Aleksey Petras <leha.petras1998@gmail.com>
2023-02-22 12:53:36 +0300

    rename const and make a short note of $correctAnswer

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -4,23 +4,19 @@ namespace BrainGames\Brain\even;
 
 use function BrainGames\Engine\startGame;
 
-use const BrainGames\Engine\NUMBER_OF_ROUNDS;
+use const BrainGames\Engine\ROUNDS_COUNT;
 
-const EVEN_RULES = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
+const RULES = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
 
 function playEven(): void
 {
-    $round = [];
-    for ($i = 0; $i < NUMBER_OF_ROUNDS; $i++) {
+    $rounds = [];
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $num = rand(1, 100);
-        if (isEven($num)) {
-            $correctAnswer = 'yes';
-        } else {
-            $correctAnswer = 'no';
-        }
-        $round[] = ['question' => $num, 'correctAnswer' => $correctAnswer];
+        $correctAnswer = isEven($num) ? 'yes' : 'no';
+        $rounds[] = ['question' => $num, 'correctAnswer' => $correctAnswer];
     }
-    startGame($round, EVEN_RULES);
+    startGame($rounds, RULES);
 }
 
 function isEven(int $num): bool
